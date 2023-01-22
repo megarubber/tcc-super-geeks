a1f8de692aa95fe47a1b1301dc8a62512e3e850e
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-22 01:16:10 -0300

    chore: finish bomb explosion impulse

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -9,13 +9,22 @@ public class Bomb : MonoBehaviour
     public float explosionForce = 10f;
     public float radius = 20f;
 
-    void Start()
-    {
-        
-    }
-
     void Explosion()
     {
+        // 6: Player Layer Mask
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach(Collider near in colliders) {
+            GameObject p = near.gameObject;
+            Rigidbody rb = near.GetComponent<Rigidbody>();
+
+            if(rb != null) {
+                rb.AddExplosionForce(explosionForce, transform.position, radius, 1f, ForceMode.Acceleration);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        Explosion();
+        Destroy(gameObject);
     }
 }
